f510a1b48c511d0651de82d2e7b471b681133d49
osipowvw <osipov.maksim2802@gmail.com>
2025-03-27 19:11:05 +0300

    Delete simple-chat and dz1

--- simple-chat/src/components/chat.js
+++
@@ -1,5 +0,0 @@
-export function goBackToChatList(chatListScreen, chatScreen, setCurrentChatId) {
-    chatScreen.style.display = 'none';
-    chatListScreen.style.display = 'block';
-    setCurrentChatId(null);
-}
--- simple-chat/src/components/switch.js
+++
@@ -1,9 +0,0 @@
-export function switchUser(currentUser, currentChatName, isSwitched) {
-    if (!isSwitched) {
-        currentUser = currentChatName;
-    } else {
-        currentUser = 'Максим';
-    }
-    isSwitched = !isSwitched;
-    return { currentUser, isSwitched };
-}
